afab5c1dd7ea25a238c0dbabab13ef2bd24069b6
Ismanol García <dev@ismanolgarcia.com>
2023-08-10 18:41:23 -0700

    fixed: packet problem

--- randomquote/vite.config.js
+++ randomquote/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['styled-components'], // Agrega otros módulos aquí si es necesario
+    },
   plugins: [react()],
 })
+
